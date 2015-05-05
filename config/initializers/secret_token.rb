e690d94658d5fd7fa0c3e6e4eaf86ff0d5ec7612
Alexandra Millatmal <alexandrajmh@gmail.com>
2015-05-05 13:03:00 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PortfolioSite::Application.config.secret_token = '1255b80d75302e32da1ff5d569bea37f671162d1292f12bcb0fae1525930b259741f29cb1257855dc47f6116fb7703abd3e551c170f83c98978dec8329b905f9'

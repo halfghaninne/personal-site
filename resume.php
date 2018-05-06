2d2f5c24baee20f96f52342cb53ea3b5c7d8e80c
Alexandra Millatmal <alexandrajmh@gmail.com>
2018-05-06 11:24:57 -0400

    resume change

--- resume.php
+++ resume.php
@@ -1,11 +1,254 @@
 <?php $active = "resume"; ?>
 <?php include('_header.php') ?>
 
-  <div class="page-content">
+  <!-- <div class="page-content">
     <div class="block-fill-height">
       <div class="container">
         <embed src="AlexandraMillatmal.Dec2017.pdf" class="block-fill-height">
       </div>
+    </div>
+  </div> -->
+
+  <div class="container">
+    <div class="block block-fill-height">
+      <!-- <div class="block-lg-middle"> -->
+        <div class="row resume-row">
+          <div class="caption resume-section">
+            <h2 class="resume-section-title">
+              Experience
+            </h2>
+
+            <div class="resume-section-role">
+              <h3 class="resume-section-role-title">
+                <span>Developer, Carbon Five</span>
+                <span class="timeline">2018 - present</span>
+              </h3>
+              <h6 class="tech-stack">
+                Python2, Django, Angular5, Sass, Bootstrap, CircleCI, Docker
+              </h6>
+              <p class="responsibilities">
+                As a client-facing consultant, employ and emulate Agile and TDD methodologies for a variety of software needs.
+              </p>
+            </div>
+
+            <div class="resume-section-role">
+              <h3 class="resume-section-role-title">
+                <span>Curriculum Consultant, Girls Who Code</span>
+                <span class="timeline">2017 - 2018</span>
+              </h3>
+              <h6 class="tech-stack">
+                HTML5, CSS3, Javascript
+              </h6>
+              <p>
+                Set learning objectives and curriculum structure for Girls Who Code's Campus Web Development program, a national tuition-based course launching in summer 2018. Determined best tools and practices for course curriculum and wrote hands-on tutorials for students on concepts such as HTML/CSS, Flexbox, JavaScript events, APIs, debugging, and project planning.
+              </p>
+            </div>
+
+            <div class="resume-section-role">
+              <h3 class="resume-section-role-title">
+                <span>Developer, Big Wheel Brigade</span>
+                <span class="timeline">2016 - 2017</span>
+              </h3>
+              <h6 class="tech-stack">
+                Ruby on Rails, Node.js, HTML5, CSS3, JavaScript, PHP, Python
+              </h6>
+              <p>
+                Developed and maintained a Rails application with more than 70k active users for a long-standing client, Catalog Choice.
+              </p> 
+              <p>
+                Mentored 46 new developers as part of the company's training program, Omaha Code School, reviewing and supervising their work in Ruby on Rails, PHP, Python, JavaScript, HTML, and CSS. Built internal scripts and applications in Node.js and Ruby to streamline school operations. Led course development of and instructed the school's part-time Web Development 101 program.
+              </p>
+              <p>
+                Represented the company on issues of empathy, diversity, and inclusivity in tech.
+              </p>
+            </div>
+
+            <div class="resume-section-role">
+              <h3 class="resume-section-role-title">
+                <span>Teaching Assistant, Omaha Code School</span>
+                <span class="timeline">2015</span>
+              </h3>
+              <h6 class="tech-stack">
+                HTML5, CSS3, Javascript, WordPress, PHP
+              </h6>
+              <p>
+                Served as a Teaching Assistant for OCS: Highlander -- a coding camp for eleven high-school students from underserved districts that focused on front-end development in HTML5, CSS3, JavaScript/jQuery, and WordPress/PHP. Mentored campers during their group capstone projects, which were judged for creativity, technical achievement, and community benefit by local tech companies.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="row resume-row">
+          <div class="caption resume-section">
+            <h2 class="resume-section-title">
+              Community
+            </h2>
+
+            <div class="resume-section-role">
+              <h3 class="resume-section-role-title">
+                <span>Mentor, Code2040</span>
+                <span class="timeline">2018 - present</span>
+              </h3>
+              <h6 class="expertise-area">
+                Diversity & Inclusion, Mentorship
+              </h6>
+              <p>
+                Provide career guidance and coaching, support, and community to a 2018 Code2040 Fellow during their summer engineering placement. 
+              </p>
+            </div>
+
+            <div class="resume-section-role">
+              <h3 class="resume-section-role-title">
+                <span>Volunteer, RailsBridge NYC</span>
+                <span class="timeline">2018 - present</span>
+              </h3>
+              <h6 class="expertise-area">
+                Diversity & Inclusion, Tech Education
+              </h6>
+              <p class="responsibilities">
+                Support students and answer questions during single-day workshops designed to introduce more women to Ruby on Rails development, free of cost.
+              </p>
+            </div>
+
+            <div class="resume-section-role">
+              <h3 class="resume-section-role-title">
+                <span>Grievance Committee Member, Mystery Code Society</span>
+                <span class="timeline">2018 - present</span>
+              </h3>
+              <h6 class="expertise-area">
+                Codes of Conduct, Diversity & Inclusion, Tech Education
+              </h6>
+              <p>
+                Address code of conduct violations for Mystery Code Society, an Omaha-based non-profit providing rigorous and free coding curriculum for women, femmes, and non-men.
+              </p>
+            </div>
+
+            <div class="resume-section-role">
+              <h3 class="resume-section-role-title">
+                <span>Instructor & Organizer, Omaha Girls Who Code</span>
+                <span class="timeline">2016 - 2017</span>
+              </h3>
+              <h6 class="expertise-area">
+                Diversity & Inclusion, Mentorship, Tech Education 
+              </h6>
+              <p>
+                Instructed multiple 6th- through 8th-grade cohorts in programming, web development, and collaborative problem-solving. The focus of the curriculum includes programming fundamentals and a self-designed project with community impact.
+              </p>
+              <p>
+                Served as an organizer for the Omaha Girls Who Code Coalition, an umbrella network for the growing number of organization chapters in the Omaha area.
+              </p>
+            </div>
+
+            <div class="resume-section-role">
+              <h3 class="resume-section-role-title">
+                <span>Co-Founder & Organizer, Omaha Emerging Developers</span>
+                <span class="timeline">2016 - 2017</span>
+              </h3>
+              <h6 class="expertise-area">
+                Events, Mentorship, Technical Speaking 
+              </h6>
+              <p>
+                Organized and implemented monthly events for the group of over 200 new and mid-level developers in which members could network, share their unique skills, and practice delivering technical talks to peers.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="row resume-row">
+          <div class="caption resume-section">
+            <h2 class="resume-section-title">
+              Education
+            </h2>
+
+            <div class="resume-section-role">
+              <h3 class="resume-section-role-title">
+                <span>Omaha Code School</span>
+                <span class="timeline">Spring 2015</span>
+              </h3>
+              <h6 class="expertise-area">
+                Accelerated Certificate Program
+              </h6>
+              <p class="responsibilities">
+                Full-stack development training in SQL, PostgreSQL, Ruby, Sinatra, Ruby on Rails, HTML5, CSS3, JavaScript, AJAX, and version control with git/Github. Additional experience with: MongoDB, Agile development, API integration, third-party authentication, and data visualization (Mapbox, Chartkick).
+              </p>
+            </div>
+
+            <div class="resume-section-role">
+              <h3 class="resume-section-role-title">
+                <span>Nebraska Wesleyan University</span>
+                <span class="timeline">2007 - 2012</span>
+              </h3>
+              <h6 class="expertise-area">
+                B.A., Political Science & Religion
+              </h6>
+              <p>
+                Minors: Mathematics and Gender Studies.
+              </p>
+              <p>
+                Semester of research in Tunis with the School for International Training, Spring 2011.<br>
+                Communications Intern for the Middle East Institute in Washington DC, Fall 2011.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="row resume-row">
+          <div class="caption resume-section">
+            <h2 class="resume-section-title">
+              References
+            </h2>
+
+            <div class="resume-section-role">
+              <h3 class="resume-section-role-title">
+                <span>Jackie Blizzard</span>
+                <ul class="contact">
+                  <!-- <li><a href="" target="_blank"><span class="icon icon-paper-plane"></span></a></li> -->
+                  <li><a href="https://www.linkedin.com/in/jackieblizzard/" target="_blank"><span class="icon icon-linkedin"></span></a></li>
+                </ul>
+              </h3>
+              <h6 class="expertise-area">
+                Former Supervisor
+              </h6>
+            </div>
+
+            <div class="resume-section-role">
+              <h3 class="resume-section-role-title">
+                <span>Sumeet Jain</span>
+                <ul class="contact">
+                  <li><a href="http://www.twitter.com/hul" target="_blank"><span class="icon icon-twitter"></span></a></li>
+                  <!-- <li><a href="" target="_blank"><span class="icon icon-paper-plane"></span></a></li> -->
+                  <li><a href="https://www.linkedin.com/in/sumeetjain/" target="_blank"><span class="icon icon-linkedin"></span></a></li>
+                </ul>
+              </h3>
+              <h6 class="expertise-area">
+                Mentor, Former Supervisor
+              </h6>
+            </div>
+
+            <div class="resume-section-role">
+              <h3 class="resume-section-role-title">
+                <span>Rahul Gupta</span>
+                <ul class="contact">
+                  <li><a href="http://www.twitter.com/hul" target="_blank"><span class="icon icon-twitter"></span></a></li>
+                  <!-- <li><a href="" target="_blank"><span class="icon icon-paper-plane"></span></a></li> -->
+                  <li><a href="https://www.linkedin.com/in/rahulgupta/" target="_blank"><span class="icon icon-linkedin"></span></a></li>
+                </ul>
+              </h3>
+              <h6 class="expertise-area">
+                Former Supervisor
+              </h6>
+            </div>
+          </div>
+        </div>
+
+        <div class="row resume-row resume-download">
+          <!-- <div class="caption resume-section"> -->
+            <a href="AlexandraMillatmal.Dec2017.pdf" download="AlexandraMillatmal.pdf" class="btn btn-pill btn-download">
+              Download <span class="icon icon-arrow-with-circle-down"></span>
+            </a>
+          <!-- </div> -->
+        </div>
+      <!-- </div> -->
     </div>
   </div>
 
